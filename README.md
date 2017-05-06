6333de6309bfe7fb94c8c8c0013c5d3f1fec282c
navegacion <jamesherrickw@gmail.com>
2017-05-06 06:21:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Glitter-Gitter
+
+Glitter gitter is a simple userscript that allows you to open someone's Free Code Camp profile by clicking their name.
+
+
+## Getting started
+
+### Install
+
+Requires either [GreaseMonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) (FireFox) or [Tampermonkey](https://tampermonkey.net/) (Chrome, Opera, Safari, MSE).
+
+You can use this [YouTube](https://www.youtube.com/watch?v=I1W2p6qLBUI) video or Google [installing userscripts](http://lmgtfy.com/?q=installing+userscripts).
+
+Once you have it installed, go to the [chat](https://gitter.im/FreeCodeCamp/Help) and you will see ninjas - like the screenshot below!
+
+![screenshot](preview.png)
+
+Red Background Ninja - Uses the name without the @.
+
+Non-Red Background Ninja - Uses the name with the @.
+
+Why? Sometimes one won't work, but the other will. There isn't any definite way to tell which one won't land a 301 at this time.
+
+# Better Content
+
+Let me deliver you better content by dropping me a few tokens of appreciation.
+
+BTC Addy: 18rQ5HzSwaFKvR54CCNhgq7eUDyN84X4Lu
+
+# License
+
+[MIT](https://opensource.org/licenses/GPL-3.0) © [navegacion](https://codepen.io/navegacion/)
---
+++ glitter-gitter.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Glitter Gitter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Open their Free Code Camp profile, instead of Github.
+// @author       navegacion
+// @match        https://gitter.im/*
+// @grant        none
+// ==/UserScript==
+
+if(typeof jQuery === 'undefined'|| !jQuery){
+    (function(){
+        var s=document.createElement('script');
+        s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js');
+        if(typeof jQuery=='undefined'){
+            document.getElementsByTagName('head')[0].appendChild(s);
+        }
+    })();
+}
+
+(function(){
+    var readd = function() {
+        $( "div.chat-item__details" ).each(function( index ) {
+            var $this = $(this);
+            $this.children("div.chat-item__from.js-chat-item-from").each(function() {
+                var name = $(this);
+                var username = $this.children("div.chat-item__username.js-chat-item-from");
+                var profile = $this.children("div#profile");
+                var url = "http://freecodecamp.com/";
+
+                var profile_one = '<span id="first"><a href="' + url + name.html() + '" target="_blank"><img style="width:20px;" src="http://i.imgur.com/ZvExY7I.png"></a></span>';
+                var profile_two = '<span id="second"><a href="' + url + username.html().substr(1) + '" target="_blank"><img style="width:20px;" src="http://i.imgur.com/vjBpVgj.png"></a></span>';
+                var profile_display = profile_one + ' ' + profile_two;
+                
+                if(!profile.length) {
+                    username.after('<div id="profile" style="padding:5px;width:8%;margin: 0 auto;">' + profile_display + '</div>');
+                }
+            });
+        });
+    };
+
+    var intervalInt = window.setInterval(function(){
+        if(typeof jQuery !== 'undefined' && jQuery){
+            readd();
+        }
+    }, 850);
+})();
